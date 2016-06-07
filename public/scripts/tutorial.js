04d4f42938f87eebde83454750065c2f1a0dc0aa
acanada <acanada@cnio.es>
2016-06-07 18:43:17 +0200

    packaje.json configuration

---
+++ public/scripts/tutorial.js
@@ -0,0 +1,53 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+
+
+
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);

1b2c35711a662228b21b001626cf971009503353
acanada <acanada@cnio.es>
2016-07-16 01:55:49 +0200

    Added User view from User List. Error in Modal and history.back

---
+++ src/app/components/UserViewForm.jsx
@@ -0,0 +1,149 @@
+var React = require('react');
+var Bootstrap = require('react-bootstrap');
+var jQuery = require('jquery');
+import Form from "react-jsonschema-form";
+import { Row, Col, Code } from 'react-bootstrap';
+//var ModalError = require("./ModalError.jsx");
+
+function userValidation(formData,errors) {
+	if (formData.userPassword !== formData.userPassword2) {
+	    errors.userPassword2.addError("Passwords don't match");
+	}
+		return errors;
+}
+
+var UserViewForm = React.createClass({
+	propTypes:{
+		schema: React.PropTypes.object.isRequired,
+		data: React.PropTypes.object.isRequired
+  	},
+  	getInitialState: function() {
+		return { error: null, showModal:false};
+	},
+	close(){
+		this.setState({showModal: false});
+	},
+	open(){
+		this.setState({showModal: true});
+	},
+  	updateUserData: function(formData){
+  		console.log("yay I'm valid!");
+  		//console.log(formData);
+  		var userData = Object.create({},formData);
+  		delete userData.userPassword2;
+  		jQuery.ajax({
+		    type: 'PUT',
+		    url: '/some/url',
+		    data: userData
+		})
+		.done(function(data) {
+		    self.clearForm()
+		})
+		.fail(function(jqXhr) {
+		    console.log('Failed to Update User Information',jqXhr);
+		    var responseText="";
+		    if (jqXhr.status === 0) {
+			    responseText='Failed to Update User Information. Not connect: Verify Network.';
+			} else if (jqXhr.status == 404) {
+			    responseText='Failed to Update User Information. Not found [404]';
+			} else if (jqXhr.status == 500) {
+			    responseText='Failed to Update User Information. Internal Server Error [500].';
+			} else if (textStatus === 'parsererror') {
+			    responseText='Failed to Update User Information. Sent JSON parse failed.';
+			} else if (textStatus === 'timeout') {
+			    responseText='Failed to Update User Information. Time out error.';
+			} else if (textStatus === 'abort') {
+			    responseText='Ajax request aborted.';
+			} else {
+			    responseText='Uncaught Error: ' + jqXHR.responseText;
+			}
+		    this.setState({error: responseText, showModal: true});
+		}.bind(this));
+  	},
+  	render: function() {
+  		var schema = this.props.schema;
+  		// Replicating userPassword for schema validation and Ordering Schema for ui:order
+  		//Adding a userPassword2 field to validate userPassword change
+  		schema.properties.userPassword2 = schema.properties.userPassword;
+  		//First we create an array with the fields with the desired order.
+  		var order = ["username","cn","givenName","surname","userPassword","userPassword2"];
+  		//We filter all the properties retrieving only the elements that are not in "order" array
+		var userSchemaKeys = Object.keys(schema.properties).filter(function(elem) {
+  			return order.indexOf(elem)==-1;
+  		});
+  		//We concatenate order with userSchemaKeys, retrieving the ordered schema as desired
+  		var schemaOrdered = order.concat(userSchemaKeys);
+		
+		var data = this.props.data;
+  		delete data.userPassword;	
+  		console.log(schema);
+  		console.log(data);
+		const uiSchema = {
+			"ui:order": schemaOrdered,
+			"userPassword": {
+				"ui:widget": "password",
+				"ui:placeholder": "************"
+			},
+			"userPassword2": {
+				"ui:widget": "password",
+				"ui:placeholder": "************"
+			},
+			"cn": {
+				"ui:readonly": true,
+			},
+			"organizationalUnit": {
+				"ui:readonly": true,
+			},
+			"groups": {
+				"ui:readonly": true,
+			},
+			"registeredAddress": {
+				"ui:widget": "textarea",
+				"type": "string"
+			},
+			"postalAddress": {
+				"ui:widget": "textarea",
+				"type": "string"
+			}
+		};
+		const log = (type) => console.log.bind(console, type);
+		const onSubmit = ({formData}) => this.updateUserData({formData});
+		const onError = (errors) => console.log("I have", errors.length, "errors to fix");
+		console.log("Error: ", this.state.error);
+		console.log("Show: ", this.state.showModal);
+		
+	    return (
+			<div>				
+				<Bootstrap.Modal show={this.state.showModal} onHide={this.close} error={this.state.error}>
+          			<Bootstrap.Modal.Header closeButton>
+            			<Bootstrap.Modal.Title>Error!</Bootstrap.Modal.Title>
+          				</Bootstrap.Modal.Header>
+          			<Bootstrap.Modal.Body>
+            			<h4>{this.state.error}</h4>
+          			</Bootstrap.Modal.Body>
+          			<Bootstrap.Modal.Footer>
+            			<Bootstrap.Button onClick={this.close}>Close</Bootstrap.Button>
+					</Bootstrap.Modal.Footer>
+				</Bootstrap.Modal>
+				<Row className="show-grid">
+      				<Col xs={12} md={8}>
+      					<code>
+      						<Form schema={schema}
+					        uiSchema={uiSchema}
+					        formData={data}
+					        onChange={log("changed")}
+					        onSubmit={onSubmit}
+					        onError={onError}
+					        validate={userValidation}
+					        liveValidate= {true} />
+      					</code>
+      				</Col>
+      				<Col xs={6} md={4}>
+      					<code></code>
+      				</Col>
+    			</Row>
+		    </div>
+	    );
+	 }
+});
+module.exports = UserViewForm;
---
+++ src/app/components/UserViewFormContainer.jsx
@@ -0,0 +1,101 @@
+var React = require('react');
+var jQuery = require('jquery');
+var UserViewForm = require('./UserEditForm.jsx');
+
+var UserViewFormContainer = React.createClass({
+  getInitialState: function() {
+    return { schema: null, data: null };
+  },
+  
+  loadUserSchema: function() {
+	  	jQuery.ajax({
+	  		url: 'json/userValidation.json',
+	  		type: 'GET',
+	  		dataType: 'json',
+	  		headers: {
+	  			'X-CAS-Referer': window.location.href
+	  		},
+	  		contentType: 'application/json; charset=utf-8',
+	  	})
+	  	.done(function(schema) {
+      		this.setState({schema: schema});
+    	}.bind(this))
+		.fail(function(jqXhr) {
+		    console.log('failed to retrieve user Schema',jqXhr);
+		    var responseText="";
+		    if (jqXhr.status === 0) {
+			    responseText='Not connect: Verify Network.';
+			} else if (jqXhr.status == 404) {
+			    responseText='Validation Schema not found [404]';
+			} else if (jqXhr.status == 500) {
+			    responseText='Internal Server Error [500].';
+			} else if (textStatus === 'parsererror') {
+			    responseText='Requested JSON parse failed.';
+			} else if (textStatus === 'timeout') {
+			    responseText='Time out error.';
+			} else if (textStatus === 'abort') {
+			    responseText='Ajax request aborted.';
+			} else {
+			    responseText='Uncaught Error: ' + jqXHR.responseText;
+			}
+		    this.setState({error: responseText});
+		}.bind(this));
+  },
+  loadUserData: function() {
+	  	jQuery.ajax({
+	  		url: 'json/user-'+this.props.params.username+'.json',
+	  		type: 'GET',
+	  		dataType: 'json',
+	  		headers: {
+	  			'X-CAS-Referer': window.location.href
+	  		},
+	  		contentType: 'application/json; charset=utf-8',
+	  	})
+  		.done(function(data) {
+  			this.setState({data: data});
+  			this.loadUserSchema();
+		}.bind(this))
+		.fail(function(jqXhr, textStatus, errorThrown) {
+		    console.log('failed to retrieve user Information',jqXhr);
+		    var responseText="";
+		    if (jqXhr.status === 0) {
+			    responseText='Not connect: Verify Network.';
+			} else if (jqXhr.status == 404) {
+			    responseText='Requested User not found [404]';
+			} else if (jqXhr.status == 500) {
+			    responseText='Internal Server Error [500].';
+			} else if (textStatus === 'parsererror') {
+			    responseText='Requested JSON parse failed.';
+			} else if (textStatus === 'timeout') {
+			    responseText='Time out error.';
+			} else if (textStatus === 'abort') {
+			    responseText='Ajax request aborted.';
+			} else {
+			    responseText='Uncaught Error: ' + jqXHR.responseText;
+			}
+		    this.setState({error: responseText});
+		}.bind(this));
+  },
+  componentDidMount: function() {
+    	this.loadUserData();
+  },
+
+  render: function() {
+    if (this.state.schema && this.state.data) {
+      return (
+      	<div>
+	    	<UserViewForm   schema={this.state.schema}  data={this.state.data}  />
+	    </div>
+      )
+    }
+    if (this.state.error) {
+      return (
+      	<div>
+	    	Error: {this.state.error}
+	    </div>
+      )
+    }
+    return <div>Loading...</div>;
+  }
+});
+module.exports = UserViewFormContainer;

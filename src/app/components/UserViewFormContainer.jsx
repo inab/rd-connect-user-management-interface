60d96962455c1d43478b69d6302722d988ab4f23
acanada <acanada@cnio.es>
2016-07-16 04:15:13 +0200

    Added error on user view to modal window

--- src/app/components/UserViewFormContainer.jsx
+++ src/app/components/UserViewFormContainer.jsx
@@ -1,13 +1,21 @@
 var React = require('react');
+var Bootstrap = require('react-bootstrap');
 var jQuery = require('jquery');
 var UserViewForm = require('./UserViewForm.jsx');
+import { Row, Col, Code } from 'react-bootstrap';
+import {ReactRouter, Router, Route, Link, History} from 'react-router';
 
 var UserViewFormContainer = React.createClass({
   getInitialState: function() {
-    return { schema: null, data: null };
-  },
-  
-  loadUserSchema: function() {
+    return { schema: null, data: null, error: null, showModal: false };
+  	},
+  	close(){
+		this.setState({showModal: false});
+	},
+	open(){
+		this.setState({showModal: true});
+	},
+  	loadUserSchema: function() {
 	  	jQuery.ajax({
 	  		url: 'json/userValidation.json',
 	  		type: 'GET',
@@ -21,24 +29,24 @@ var UserViewFormContainer = React.createClass({
       		this.setState({schema: schema});
     	}.bind(this))
 		.fail(function(jqXhr) {
-		    console.log('failed to retrieve user Schema',jqXhr);
+		    console.log('Failed to retrieve user Schema',jqXhr);
 		    var responseText="";
 		    if (jqXhr.status === 0) {
-			    responseText='Not connect: Verify Network.';
+			    responseText='Failed to retrieve user Schema. Not connect: Verify Network.';
 			} else if (jqXhr.status == 404) {
-			    responseText='Validation Schema not found [404]';
+			    responseText='Failed to retrieve user Schema. Validation Schema not found [404]';
 			} else if (jqXhr.status == 500) {
-			    responseText='Internal Server Error [500].';
+			    responseText='Failed to retrieve user Schema. Internal Server Error [500].';
 			} else if (textStatus === 'parsererror') {
-			    responseText='Requested JSON parse failed.';
+			    responseText='Failed to retrieve user Schema. Requested JSON parse failed.';
 			} else if (textStatus === 'timeout') {
-			    responseText='Time out error.';
+			    responseText='Failed to retrieve user Schema. Time out error.';
 			} else if (textStatus === 'abort') {
-			    responseText='Ajax request aborted.';
+			    responseText='Failed to retrieve user Schema. Ajax request aborted.';
 			} else {
-			    responseText='Uncaught Error: ' + jqXHR.responseText;
+			    responseText='Failed to retrieve user Schema. Uncaught Error: ' + jqXHR.responseText;
 			}
-		    this.setState({error: responseText});
+		    this.setState({error: responseText, showModal: true});
 		}.bind(this));
   },
   loadUserData: function() {
@@ -56,30 +64,30 @@ var UserViewFormContainer = React.createClass({
   			this.loadUserSchema();
 		}.bind(this))
 		.fail(function(jqXhr, textStatus, errorThrown) {
-		    console.log('failed to retrieve user Information',jqXhr);
+		    console.log('Failed to retrieve user Information',jqXhr);
 		    var responseText="";
 		    if (jqXhr.status === 0) {
-			    responseText='Not connect: Verify Network.';
+			    responseText='Failed to retrieve user Information. Not connect: Verify Network.';
 			} else if (jqXhr.status == 404) {
-			    responseText='Requested User not found [404]';
+			    responseText='Failed to retrieve user Information. Requested User not found [404]';
 			} else if (jqXhr.status == 500) {
-			    responseText='Internal Server Error [500].';
+			    responseText='Failed to retrieve user Information. Internal Server Error [500].';
 			} else if (textStatus === 'parsererror') {
-			    responseText='Requested JSON parse failed.';
+			    responseText='Failed to retrieve user Information. Requested JSON parse failed.';
 			} else if (textStatus === 'timeout') {
-			    responseText='Time out error.';
+			    responseText='Failed to retrieve user Information. Time out error.';
 			} else if (textStatus === 'abort') {
-			    responseText='Ajax request aborted.';
+			    responseText='Failed to retrieve user Information. Ajax request aborted.';
 			} else {
-			    responseText='Uncaught Error: ' + jqXHR.responseText;
+			    responseText='Failed to retrieve user Information. Uncaught Error: ' + jqXHR.responseText;
 			}
-		    this.setState({error: responseText});
+		    this.setState({error: responseText, showModal: true});
 		}.bind(this));
   },
   componentDidMount: function() {
     	this.loadUserData();
   },
-
+  mixins: [ History ], //This is to browse history back when user is not found after showing modal error
   render: function() {
     if (this.state.schema && this.state.data) {
       return (
@@ -91,7 +99,17 @@ var UserViewFormContainer = React.createClass({
     if (this.state.error) {
       return (
       	<div>
-	    	Error: {this.state.error}
+	    	<Bootstrap.Modal show={this.state.showModal} onHide={this.history.goBack} error={this.state.error}>
+      			<Bootstrap.Modal.Header closeButton>
+        			<Bootstrap.Modal.Title>Error!</Bootstrap.Modal.Title>
+      				</Bootstrap.Modal.Header>
+      			<Bootstrap.Modal.Body>
+        			<h4>{this.state.error}</h4>
+      			</Bootstrap.Modal.Body>
+      			<Bootstrap.Modal.Footer>
+        			<Bootstrap.Button onClick={this.history.goBack}>Close</Bootstrap.Button>
+				</Bootstrap.Modal.Footer>
+			</Bootstrap.Modal>
 	    </div>
       )
     }

0e193ef925480d5f2795755b9f17ad6aa5c09649
acanada <acanada@cnio.es>
2016-07-16 02:03:37 +0200

    Clean NavDropdown

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -18,28 +18,22 @@ var Navigation = React.createClass({
                     <NavDropdown id="NavDropdown-Users" eventKey={2} title="Users">
                         <LinkContainer to="/userList"><MenuItem eventKey="1">List Users</MenuItem></LinkContainer>
                         <LinkContainer to="/userNew"><MenuItem eventKey="2">New User</MenuItem></LinkContainer>
-                        <LinkContainer to="/userAction3"><MenuItem eventKey="3">Users Action 3</MenuItem></LinkContainer>
-                        <LinkContainer to="/userAction4"><MenuItem eventKey="4">Users Action 4</MenuItem></LinkContainer>
                         <MenuItem divider />
-                        <LinkContainer to="/userAction5"><MenuItem eventKey="5">Separated Users link</MenuItem></LinkContainer>
+                        <LinkContainer to="/userAction5"><MenuItem eventKey="3">Separated Users link</MenuItem></LinkContainer>
 
                     </NavDropdown>
                     <NavDropdown id="NavDropdown-Organizations" eventKey={3} title="Organizations">
                         <LinkContainer to="/organizationList"><MenuItem eventKey="1">List Organizations</MenuItem></LinkContainer>
                         <LinkContainer to="/organizationNew"><MenuItem eventKey="2">New Organization</MenuItem></LinkContainer>
-                        <LinkContainer to="/organizationAction3"><MenuItem eventKey="3">Organizations Action 3</MenuItem></LinkContainer>
-                        <LinkContainer to="/organizationAction4"><MenuItem eventKey="4">Organizations Action 4</MenuItem></LinkContainer>
                         <MenuItem divider />
-                        <LinkContainer to="/organizationAction5"><MenuItem eventKey="5">Separated Organizations link</MenuItem></LinkContainer>
+                        <LinkContainer to="/organizationAction5"><MenuItem eventKey="3">Separated Organizations link</MenuItem></LinkContainer>
                         
                     </NavDropdown>
                     <NavDropdown id="NavDropdown-Groups" eventKey={4} title="Groups">
                         <LinkContainer to="/groupList"><MenuItem eventKey="1">List Groups</MenuItem></LinkContainer>
                         <LinkContainer to="/groupNew"><MenuItem eventKey="2">New Group</MenuItem></LinkContainer>
-                        <LinkContainer to="/groupAction3"><MenuItem eventKey="3">Groups Action 3</MenuItem></LinkContainer>
-                        <LinkContainer to="/groupAction4"><MenuItem eventKey="4">Groups Action 4</MenuItem></LinkContainer>
                         <MenuItem divider />
-                        <LinkContainer to="/groupAction5"><MenuItem eventKey="5">Separated Groups link</MenuItem></LinkContainer>
+                        <LinkContainer to="/groupAction5"><MenuItem eventKey="3">Separated Groups link</MenuItem></LinkContainer>
                     </NavDropdown>
                 </Nav>
             </Navbar>

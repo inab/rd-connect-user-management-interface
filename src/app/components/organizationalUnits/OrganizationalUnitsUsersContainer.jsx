690ad345d211146c9336399b1d8b4712388060aa
José María Fernández <jose.m.fernandez@bsc.es>
2018-07-12 18:17:36 +0200

    OrganizationalUnitsUsersContainer has been refactored

--- src/app/components/organizationalUnits/OrganizationalUnitsUsersContainer.jsx
+++ src/app/components/organizationalUnits/OrganizationalUnitsUsersContainer.jsx
@@ -1,59 +1,53 @@
 import React from 'react';
-import jQuery from 'jquery';
 import OrganizationalUnitsUsers from './OrganizationalUnitsUsers.jsx';
 
-import config from 'config.jsx';
+import AbstractFetchedDataContainer from '../AbstractUMContainer.jsx';
 
-var OrganizationalUnitsUsersContainer = React.createClass({
-	propTypes:{
-		task: React.PropTypes.string.isRequired
-	},
-	getInitialState: function() {
-		return {
-			data: []
-		};
-
-	},
-	componentWillMount: function() {
-		this.loadUsersFromServer();
-		this.loadUsersInterval = setInterval(this.loadUsersFromServer, 20000);
-	},
-	componentWillUnmount: function(){
-		clearInterval(this.loadUsersInterval);
-		this.serverRequest.abort();
-	},
-	loadUsersFromServer: function() {
-			this.serverRequest = jQuery.ajax({
-				url: config.usersBaseUri,
-				dataType: 'json',
-				cache: false,
-				success: function(data) {
-					//console.log('success!');
-					this.setState({data: data});
-					//console.log(this.state.data);
-				}.bind(this),
-				error: function(xhr, status, err) {
-					//console.error('json/users.json', status, err);
-					console.error(xhr.status);
-					this.setState({error: xhr.status + ' (Retrieving users)'});
-				}.bind(this)
+class OrganizationalUnitsUsersContainer extends AbstractFetchedDataContainer {
+	constructor(props,context) {
+		super(props,context);
+		this.history = props.history;
+	}
+	
+	componentWillMount() {
+		super.componentWillMount();
+		
+		this.setState({
+			users: []
+		});
+	}
+	
+	componentDidMount() {
+		let errHandler = (err) => {
+			this.onChange({
+				...err,
+				showModal: true
 			});
-	},
-	render: function() {
+		};
+		
+		this.usersPromise()
+			.then((users) => {
+				//console.log('success!');
+				this.setState({users: users});
+				//console.log(this.state.users);
+			}, errHandler);
+	}
+	
+	render() {
 		if(this.state.error) {
 			return (
 				<div>Error {this.state.error}</div>
 			);
 		}
-		if(this.state.data) {
+		if(this.state.users) {
 			return (
 				<div>
-					<OrganizationalUnitsUsers data={this.state.data} task="viewEdit"/>
+					<OrganizationalUnitsUsers data={this.state.users} task="viewEdit"/>
 				</div>
 			);
 		}
 		return <div>Loading...</div>;
 	}
-});
+}
 
-module.exports = OrganizationalUnitsUsersContainer;
+export default OrganizationalUnitsUsersContainer;

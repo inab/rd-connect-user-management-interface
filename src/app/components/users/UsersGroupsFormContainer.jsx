0365169f6cb1d255e49bcfb724f1ef4d11a5662c
José María Fernández <jose.m.fernandez@bsc.es>
2018-07-13 03:18:41 +0200

    Major code refactoring, so the shared data management calls are used

--- src/app/components/users/UsersGroupsFormContainer.jsx
+++ src/app/components/users/UsersGroupsFormContainer.jsx
@@ -1,117 +1,71 @@
 import React from 'react';
-import jQuery from 'jquery';
 import UsersGroupsEditForm from './UsersGroupsEditForm.jsx';
 import UsersGroupsViewForm from './UsersGroupsViewForm.jsx';
+import AbstractFetchedDataContainer from '../AbstractUMContainer.jsx';
 
-import config from 'config.jsx';
-
-var UsersGroupsFormContainer = React.createClass({
-	propTypes:{
-		route: React.PropTypes.array,
-		params: React.PropTypes.array
-	},
-	getInitialState: function() {
-		//console.log('TASK: ', this.props.route.task);
-		return {
+class UsersGroupsFormContainer extends AbstractFetchedDataContainer {
+	constructor(props,context) {
+		super(props,context);
+		this.history = props.history;
+	}
+	
+	componentWillMount() {
+		super.componentWillMount();
+		this.setState({
 			schema: null,
 			data: null,
 			groups: null,
 			error: null,
 			showModal: false,
-			task: null
-		};
-	},
-	componentWillMount: function() {
-		this.setState({task: this.props.route.task});
-		this.loadUserSchema();
-	},
-	loadUserFromServer: function() {
-		jQuery.ajax({
-			url: config.usersBaseUri + '/' + encodeURIComponent(this.props.params.username),
-			dataType: 'json',
-			cache: false,
-		})
-		.done(function(data) {
-				//console.log('success!');
+			task: this.props.route.task
+		});
+	}
+	
+	componentDidMount() {
+		// The value is already populated by this method
+		let errHandler = (err) => this.setState({error: err.status + ' (Retrieving groups)'});
+		
+		this.usersSchemaPromise()
+			.then((schema) => {
+				this.setState({schema: schema});
+				
+				return this.userPromise(this.props.params.username);
+			},errHandler)
+			.then((data) => {
 				this.setState({data: data});
-				//console.log(this.state.data);
-		}.bind(this))
-		.fail(function(xhr, status, err) {
-				//console.error('json/users.json', status, err);
-				console.error(xhr.status);
-				this.setState({error: xhr.status + ' (Retrieving users)'});
-		}.bind(this));
-	},
-	loadGroupsFromServer: function() {
-		jQuery.ajax({
-			url: config.groupsBaseUri,
-			dataType: 'json',
-			cache: false,
-		})
-		.done(function(groups) {
-			//console.log('success!');
-			this.setState({groups: groups});
-			this.loadUserFromServer();
-		}.bind(this))
-		.fail(function(xhr, status, err) {
-			//console.error('json/OrganizationalUnitalUnits.json', status, err);
-			console.error(xhr.status);
-			this.setState({error: xhr.status + ' (Retrieving Groups)'});
-		}.bind(this));
-	},
-	loadUserSchema: function() {
-		jQuery.ajax({
-			url: config.usersBaseUri + '?schema',
-			type: 'GET',
-			dataType: 'json',
-		})
-		.done(function(schema) {
-			this.setState({schema: schema});
-			this.loadGroupsFromServer();
-		}.bind(this))
-		.fail(function(jqXhr) {
-			//console.log('failed to retrieve user Schema',jqXhr);
-			var responseText = '';
-			if(jqXhr.status === 0) {
-				responseText = 'Not connect: Verify Network.';
-			} else if(jqXhr.status === 404) {
-				responseText = 'Validation Schema not found [404]';
-			} else if(jqXhr.status === 500) {
-				responseText = 'Internal Server Error [500].';
-			} else if(jqXhr.status === 'parsererror') {
-				responseText = 'Requested JSON parse failed.';
-			} else if(jqXhr.status === 'timeout') {
-				responseText = 'Time out error.';
-			} else if(jqXhr.status === 'abort') {
-				responseText = 'Ajax request aborted.';
-			} else {
-				responseText = 'Uncaught Error: ' + jqXhr.responseText;
-			}
-			this.setState({error: responseText});
-		}.bind(this));
-	},
-
-	render: function() {
+				
+				return this.groupsPromise();
+			}, errHandler)
+			.then((groups) => {
+				this.setState({groups: groups});
+			},errHandler);
+	}
+	
+	render() {
 		//console.log('Schema: ', this.state.schema);
 		//console.log('Data: ', this.state.data);
 		//console.log('Groups: ', this.state.groups);
 		//console.log('TASK: ', this.state.task);
 		if((this.state.schema) && (this.state.data) && (this.state.groups)) {
-			if(this.state.task === 'users_groups_view'){
-				return (
-					<div>
-						<UsersGroupsViewForm schema={this.state.schema} data={this.state.data} groups={this.state.groups} />
-					</div>
-				);
-			} else if(this.state.task === 'users_groups_edit'){
-				return (
-					<div>
-						<UsersGroupsEditForm schema={this.state.schema} data={this.state.data} groups={this.state.groups} />
-					</div>
-				);
+			switch(this.state.task) {
+				case 'users_groups_view':
+					return (
+						<div>
+							<UsersGroupsViewForm schema={this.state.schema} data={this.state.data} groups={this.state.groups} history={this.history} />
+						</div>
+					);
+				case 'users_groups_edit':
+					return (
+						<div>
+							<UsersGroupsEditForm schema={this.state.schema} data={this.state.data} groups={this.state.groups} history={this.history} />
+						</div>
+					);
+				default:
+					this.state.error = 'ASSERTION: unknown state ' + this.state.task;
+					break;
 			}
-
 		}
+		
 		if(this.state.error) {
 			return (
 				<div>
@@ -119,7 +73,15 @@ var UsersGroupsFormContainer = React.createClass({
 				</div>
 			);
 		}
+		
 		return <div>Loading...</div>;
 	}
-});
-module.exports = UsersGroupsFormContainer;
+}
+
+UsersGroupsFormContainer.propTypes = {
+	route: React.PropTypes.array,
+	params: React.PropTypes.array
+};
+
+
+export default UsersGroupsFormContainer;

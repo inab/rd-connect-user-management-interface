993b12437b60c2786a2925228e0206b79445d372
José María Fernández <jose.m.fernandez@bsc.es>
2018-07-13 03:13:40 +0200

    UserManagement methods now return Promises

--- src/app/components/UserManagement.jsx
+++ src/app/components/UserManagement.jsx
@@ -6,89 +6,87 @@ class UserManagement {
 	constructor() {
 	}
 	
-	createUser(user,cb,ecb) {
+	createUserPromise(user) {
 		//console.log('inside addMembersToGroup, members contains: ',formData.members);
-		return jQuery.ajax({
-			type: 'PUT',
-			url: config.usersBaseUri,
-			headers: auth.getAuthHeaders(),
-			contentType: 'application/json',
-			data: JSON.stringify(user)
-		})
-		.done(cb)
-		.fail((jqXhr) => {
-			//console.log('Failed to add members to Group ',jqXhr);
-			let responseText = 'Failed to create user. ';
-			switch(jqXhr.status) {
-				case 0:
-					responseText += 'Not connect: Verify Network.';
-					break;
-				case 404:
-					responseText += 'Not found [404]';
-					break;
-				case 500:
-					responseText += 'Internal Server Error [500].';
-					break;
-				case 'parsererror':
-					responseText += 'Sent JSON parse failed.';
-					break;
-				case 'timeout':
-					responseText += 'Time out error.';
-					break;
-				case 'abort':
-					responseText += 'Ajax request aborted.';
-					break;
-				default:
-					responseText += 'Uncaught Error: ' + jqXhr.responseText;
-					break;
-			}
-			
-			if(ecb) {
-				ecb({ modalTitle: 'Error', error: responseText});
-			}
+		return new Promise((resolve,reject) => {
+			jQuery.ajax({
+				type: 'PUT',
+				url: config.usersBaseUri,
+				headers: auth.getAuthHeaders(),
+				contentType: 'application/json',
+				data: JSON.stringify(user)
+			})
+			.done(resolve)
+			.fail((jqXhr) => {
+				//console.log('Failed to add members to Group ',jqXhr);
+				let responseText = 'Failed to create user. ';
+				switch(jqXhr.status) {
+					case 0:
+						responseText += 'Not connect: Verify Network.';
+						break;
+					case 404:
+						responseText += 'Not found [404]';
+						break;
+					case 500:
+						responseText += 'Internal Server Error [500].';
+						break;
+					case 'parsererror':
+						responseText += 'Sent JSON parse failed.';
+						break;
+					case 'timeout':
+						responseText += 'Time out error.';
+						break;
+					case 'abort':
+						responseText += 'Ajax request aborted.';
+						break;
+					default:
+						responseText += 'Uncaught Error: ' + jqXhr.responseText;
+						break;
+				}
+				
+				reject({ modalTitle: 'Error', error: responseText});
+			});
 		});
 	}
 	
-	modifyUser(username,user,cb,ecb) {
-		//console.log('inside addMembersToGroup, members contains: ',formData.members);
-		return jQuery.ajax({
-			type: 'POST',
-			url: config.composeUserURI(username),
-			headers: auth.getAuthHeaders(),
-			contentType: 'application/json',
-			data: JSON.stringify(user)
-		})
-		.done(cb)
-		.fail((jqXhr) => {
-			//console.log('Failed to add members to Group ',jqXhr);
-			let responseText = 'Failed to modify user ' + username + '. ';
-			switch(jqXhr.status) {
-				case 0:
-					responseText += 'Not connect: Verify Network.';
-					break;
-				case 404:
-					responseText += 'Not found [404]';
-					break;
-				case 500:
-					responseText += 'Internal Server Error [500].';
-					break;
-				case 'parsererror':
-					responseText += 'Sent JSON parse failed.';
-					break;
-				case 'timeout':
-					responseText += 'Time out error.';
-					break;
-				case 'abort':
-					responseText = 'Ajax request aborted.';
-					break;
-				default:
-					responseText = 'Uncaught Error: ' + jqXhr.responseText;
-					break;
-			}
-			
-			if(ecb) {
-				ecb({ modalTitle: 'Error', error: responseText});
-			}
+	modifyUserPromise(username,user) {
+		return new Promise((resolve,reject) => {
+			jQuery.ajax({
+				type: 'POST',
+				url: config.composeUserURI(username),
+				headers: auth.getAuthHeaders(),
+				contentType: 'application/json',
+				data: JSON.stringify(user)
+			})
+			.done(resolve)
+			.fail((jqXhr) => {
+				let responseText = 'Failed to modify user ' + username + '. ';
+				switch(jqXhr.status) {
+					case 0:
+						responseText += 'Not connect: Verify Network.';
+						break;
+					case 404:
+						responseText += 'Not found [404]';
+						break;
+					case 500:
+						responseText += 'Internal Server Error [500].';
+						break;
+					case 'parsererror':
+						responseText += 'Sent JSON parse failed.';
+						break;
+					case 'timeout':
+						responseText += 'Time out error.';
+						break;
+					case 'abort':
+						responseText = 'Ajax request aborted.';
+						break;
+					default:
+						responseText = 'Uncaught Error: ' + jqXhr.responseText;
+						break;
+				}
+				
+				reject({ modalTitle: 'Error', error: responseText});
+			});
 		});
 	}
 }
